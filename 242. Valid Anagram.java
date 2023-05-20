e9fc43625bae5aa1bb4e57b4196874fade21c85b
punyamsingh <89277920+punyamsingh@users.noreply.github.com>
2023-05-20 21:05:53 +0530

    Create 242. Valid Anagram.java

---
+++ 242. Valid Anagram.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length()!=t.length())
+            return false;
+
+        Map<Character, Integer> charCount = new HashMap<>();
+
+        for (char c : s.toCharArray()) {
+            charCount.put(c, charCount.getOrDefault(c, 0) + 1);
+        }
+        
+        for (char c : t.toCharArray()) {
+            if (!charCount.containsKey(c) || charCount.get(c) <= 0) {
+                return false;
+            }
+            charCount.put(c, charCount.get(c) - 1);
+        }
+        return true;
+    }
+}
