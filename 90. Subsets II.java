7019888ea3cbd270667523d294fc37c133c4e8da
Punyam Singh <89277920+punyamsingh@users.noreply.github.com>
2024-05-22 00:48:33 +0530

    Update 90. Subsets II.java

--- 90. Subsets II.java
+++ 90. Subsets II.java
@@ -1,32 +1,25 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        List<List<Integer>> ans= new ArrayList<>();
-        Arrays.sort(nums);
-        f(nums, 0, new int[nums.length], 0,ans);
-        return ans;
-        //path[] go into cur, cur go into ans
-        // 下一组的第一个数的位置
-        // 当前数x，要0个 // 当前数x，要1个、要2个、要3个...都尝试
-    }
-    public static void f(int[] nums, int i, int[ ] path, int size, List<List<Integer>> ans){
-        if(i==nums.length){
-            List<Integer> cur=new ArrayList<>();
-            for(int j=0;j<size;j++){
-cur.add(path[j]);//???????
-            }            
-            ans.add(cur);
-        }else{
-            int j=i+1;
-            while(j<nums.length && nums[j]==nums[i]){
-                j++;
-            }
-            f(nums, j,path, size, ans);
-            for(;i<j;i++){
-                path[size++]=nums[i];
-                f(nums, j,path, size, ans);
-            }
+        Arrays.sort(nums); // Sort the input array to handle duplicates
+        List<List<Integer>> result = new ArrayList<>();
+        result.add(new ArrayList<>()); // Start with the empty subset
 
+        int start, end = 0; // Initialize end here
+        for (int i = 0; i < nums.length; i++) {
+            start = 0;
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                start = end + 1;
+            }
+            end = result.size() - 1;
+
+            int size = result.size();
+            for (int j = start; j < size; j++) {
+                List<Integer> currentSubset = new ArrayList<>(result.get(j));
+                currentSubset.add(nums[i]);
+                result.add(currentSubset);
+            }
         }
 
+        return result;
     }
 }
